403566580169f104bea5dc04350ed71a4c91e9ba
RegiLoshi <rloshi111@gmail.com>
2024-09-15 19:20:12 +0200

    vercel

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import User from '../models/user.js';
-import bcrypt from 'bcrypt'; 
 import jwt from 'jsonwebtoken';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
